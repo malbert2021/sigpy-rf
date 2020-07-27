005c5370799bfc4f7a274274e0a1fbb68d78a8ee
jonbmartin <jon.bach.martin@gmail.com>
2020-07-27 17:58:26 -0500

    add early version small-tip spokes ptx designer, update required imports

--- sigpy/mri/rf/ptx.py
+++ sigpy/mri/rf/ptx.py
@@ -4,10 +4,14 @@
 """
 
 import sigpy as sp
+import numpy as np
+
 from sigpy.mri import rf as rf
 from sigpy import backend
+from scipy.interpolate import interp1d
 
-__all__ = ['stspa']
+
+__all__ = ['stspa', 'stspk']
 
 
 def stspa(target, sens, coord, dt, roi=None, alpha=0, b0=None, tseg=None,
@@ -109,3 +113,162 @@ def stspa(target, sens, coord, dt, roi=None, alpha=0, b0=None, tseg=None,
         if st is not None:
             pulses = xp.reshape(alg_method.x, [Nc, Nt])
         return pulses
+
+
+def stspk(mask, sens, n_spokes, fov, dx_max, gts, sl_thick, tbw, dgdtmax, gmax,
+         alpha=0, max_cg_iter=10, tol=1E-6):
+    """Small tip spokes and k-t points parallel transmit pulse designer. A
+        draft to be improved upon.
+
+       Args:
+           mask (ndarray): region in which to optimize flip angle uniformity
+               in slice. [dim dim]
+           sens (ndarray): sensitivity maps. [Nc dim dim]
+           n_spokes (int): number of spokes to be created in the design.
+           fov (float): excitation FOV (cm).
+           dx_max (float): max. resolution of the trajectory (cm).
+           gts (float): hardware sampling dwell time (s).
+           sl_thick (float): slice thickness (cm).
+           tbw (int): time-bandwidth product.
+           dgdtmax (float): max gradient slew (g/cm/s).
+           gmax (float): max gradient amplitude (g/cm).
+           alpha (float): regularization parameter.
+           max_cg_iter (int): max number of cg iterations used during RF weight
+               design for each spoke
+           tol (float): allowable error.
+
+    Returns:
+        2-element tuple containing
+
+        - **pulses** (*array*): RF waveform out.
+        - **g** (*array*): corresponding gradient, in g/cm.
+
+       References:
+           Grissom, W., Khalighi, M., Sacolick, L., Rutt, B. & Vogel, M (2012).
+           Small-tip-angle spokes pulse design using interleaved greedy and
+           local optimization methods. Magnetic Resonance in Medicine, 68(5),
+           1553-62.
+       """
+
+    Nc = sens.shape[0]
+    dim = sens.shape[1]
+
+    kmax = 1 / dx_max  # /cm, max spatial freq of trajectory
+    # greedy kx, ky grid
+    kxs, kys = np.meshgrid(
+        np.linspace(-kmax / 2, kmax / 2 - 1 / fov, fov * kmax),
+        np.linspace(-kmax / 2, kmax / 2 - 1 / fov, fov * kmax))
+    # vectorize the grid
+    kxs = kxs.flatten()
+    kys = kys.flatten()
+
+    dc = np.intersect1d(np.where((kxs == 0)), np.where((kys == 0)))[0]
+
+    # remove DC
+    kxs = np.concatenate([kxs[:dc], kxs[dc + 1:]])
+    kys = np.concatenate([kys[:dc], kys[dc + 1:]])
+
+    # step 2: design the weights
+    kx, ky = np.zeros(1), np.zeros(1)
+    k = np.expand_dims(np.concatenate((kx, ky)), 0)
+
+    # initial target phase
+    phs = np.zeros(np.shape(mask))
+
+    for ii in range(n_spokes):
+
+        # build Afull
+        A = rf.PtxSpatialExplicit(sens, k, gts, mask.shape)
+        M = sp.linop.MatMul(A.oshape, mask)
+        A = M * A
+
+        # design wfull using MLS
+        I = sp.linop.Identity((Nc, k.shape[0]))
+        b = A.H * np.exp(1j * phs)
+        w_full = np.zeros(A.ishape, dtype=np.complex)
+        alg_method = sp.alg.ConjugateGradient(A.H * A + alpha * I,
+                                              b, w_full, P=None,
+                                              max_iter=max_cg_iter,
+                                              tol=tol)
+
+        while not alg_method.done():
+            alg_method.update()
+
+        # add a spoke using greedy method
+        if ii < n_spokes - 1:
+
+            r = np.exp(1j * phs) - A * w_full
+
+            rfnorm = np.zeros(kxs.shape)
+            for jj in range(kxs.size):
+                Afull = rf.PtxSpatialExplicit(sens, k, gts, mask.shape)
+                rfnorm[jj] = np.linalg.norm(
+                    np.linalg.pinv((Afull.H * Afull) * (Afull.H * r)))
+
+            ind = np.argmax(rfnorm)
+            k_new = np.zeros((1, 2))
+            k_new[0, 0] = kxs[ind]
+            k_new[0, 1] = kys[ind]
+            if ii % 2 != 0:  # add to end of pulse
+                k = np.concatenate((k, k_new))
+            else:  # add to beginning of pulse
+                k = np.concatenate((k_new, k))
+
+            # remove chosen point from candidates
+            kxs = np.concatenate([kxs[:ind], kxs[ind + 1:]])
+            kys = np.concatenate([kys[:ind], kys[ind + 1:]])
+
+    # from our spoke selections, build the whole waveforms
+    area = tbw / (sl_thick / 10) / 4257  # thick * kwid = twb, kwid = gam*area
+    [subgz, nramp] = rf.min_trap_grad(area, gmax, dgdtmax, gts)
+    npts = 128
+    subrf = rf.dzrf(npts, tbw, 'st')
+
+    n_plat = subgz.size - 2 * nramp  # time points on trap plateau
+    # interpolate to stretch out waveform to appropriate length
+    f = interp1d(np.arange(0, npts, 1) / npts, subrf, fill_value='extrapolate')
+    subrf = f(np.arange(0, n_plat, 1) / n_plat)
+    subrf = np.concatenate((np.zeros((nramp)), subrf, np.zeros((nramp))))
+
+    # calc gradient, add extra 0 location at end for return to (0, 0)
+    gxarea = np.diff(np.concatenate((k[:, 0], np.zeros(1)))) / 4257
+    gyarea = np.diff(np.concatenate((k[:, 1], np.zeros(1)))) / 4257
+
+    gx, gy, gz = [], [], []
+    gz_sign = -1
+    for ii in range(n_spokes):
+        gz_sign *= -1
+        gz.extend(np.squeeze(gz_sign * subgz).tolist())  # alt sign of gz
+
+        gx.extend([0] * np.size(subgz))  # zeros for gz duration
+        if np.absolute(gxarea[ii]) > 0:
+            [gblip, _] = rf.trap_grad(abs(gxarea[ii]), gmax, dgdtmax, gts)
+            gxblip = np.int(np.sign(gxarea[ii])) * gblip
+            gx = gx[:len(gx) - len(gxblip.T)]
+            gx.extend(np.squeeze(gxblip).tolist())
+
+        gy.extend([0] * np.size(subgz))
+        if np.absolute(gyarea[ii]) > 0:
+            [gblip, _] = rf.trap_grad(abs(gyarea[ii]), gmax, dgdtmax, gts)
+            gyblip = np.int(np.sign(gyarea[ii])) * gblip
+            gy = gy[:len(gy) - len(gyblip.T)]
+            gy.extend(np.squeeze(gyblip).tolist())
+
+    [gref, _] = rf.trap_grad(gts * np.sum(subgz) / 2, gmax, dgdtmax, gts)
+    gzref = - gref
+    gz.extend(np.squeeze(gzref).tolist())
+    gx.extend([0] * np.size(gzref))
+    gy.extend([0] * np.size(gzref))
+
+    pulses = np.kron(w_full, subrf)
+
+    # add zeros for gzref
+    rf_ref = np.zeros((Nc, len(gzref.T)))
+    pulses = np.concatenate((pulses, rf_ref), 1)
+
+    # combine gradient waveforms
+    gx = np.array(gx)
+    g = np.vstack((np.array(gx), np.array(gy), np.array(gz)))
+
+    return pulses, g
+
