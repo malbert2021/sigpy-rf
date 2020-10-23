e23273c0cd926f7f5d0e993b6fdb60c7f09a3252
jonbmartin <jon.bach.martin@gmail.com>
2020-10-23 15:05:54 -0500

    test structure for io.py - tests not yet implemented.

---
+++ tests/mri/rf/test_io.py
@@ -0,0 +1,21 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestIo(unittest.TestCase):
+
+    def test_signa(self):
+        print('IO Test not implemented')
+        # TODO: insert test
+
+    def test_ge_rf_params(self):
+        print('IO Test not implemented')
+        # TODO: insert testing
+
+    def test_philips_rf_params(self):
+        print('IO Test not implemented')
+        # TODO: insert testing
