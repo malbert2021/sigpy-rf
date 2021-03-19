829057674fb8bd439e79253ee6857977f08a0c69
jonbmartin <jon.bach.martin@gmail.com>
2021-03-19 14:22:32 -0500

    Rename epi.py to ep.py - was producing errors on __init__

--- sigpy/mri/rf/ep.py
+++ sigpy/mri/rf/ep.py
@@ -2,11 +2,12 @@
 """Pulse Designers for Echo Planar Imaging Excitations
 
 """
+import numpy as np
 
 __all__ = ['dz_shutters']
 
 
 def dz_shutters():
+    print('Not Implemented')
     raise NotImplementedError
-
 
